a1971ca5e9bd63309a1d0ef98198ff700e98ce47
Mark Chen <dashtotherock@gmail.com>
2026-02-17 14:00:30 -0700

    change to webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // VS Code extensions run in a Node.js-context
+  mode: 'production', // This leaves the source code as close as possible to the original
+
+  entry: './src/extension.ts', // The entry point of this extension
+  output: {
+    // The bundle is stored in the 'dist' folder (check package.json)
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    clean: true // Clean the output directory before emit
+  },
+  externals: {
+    vscode: 'commonjs vscode' // The vscode-module is created on-the-fly and must be excluded
+  },
+  resolve: {
+    // Support reading TypeScript and JavaScript files
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  infrastructureLogging: {
+    level: "log", // Enables logging required for problem matchers
+  },
+};
+
+module.exports = config;
